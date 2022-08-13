cf0325400e30babf33fb77b869a7a2612e5f928a
Dan Kelly <dpk5e7@yahoo.com>
2022-08-12 22:54:26 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// TODO: Include packages needed for this application
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  // Call generateMarkdown which returns a string
+
+  // write that string to a file
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  // This is where to do all the inquirer stuff and store it into data
+
+  // then call writeToFile
+}
+
+// Function call to initialize app
+init();
